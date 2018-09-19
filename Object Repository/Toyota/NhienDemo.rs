93ea71105e5f28e334bcc6c869c53b0881374a68
Nhien Do <Nhien Do@DESKTOP-SBNKR2D>
2018-09-19 23:20:31 +0700

    Add new code to TestBranch

---
+++ Object Repository/Toyota/NhienDemo.rs
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>NhienDemo</name>
+   <tag></tag>
+   <elementGuidId>db88e7cc-f214-4e83-9844-8f36260bde84</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
