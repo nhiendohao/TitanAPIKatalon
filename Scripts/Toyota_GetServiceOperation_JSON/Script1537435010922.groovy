12c61ad0c75bd64a4a8c8d83668cc071dd5d97f8
ANH THY <ANH THY@RENLE>
2018-09-20 22:28:19 +0700

    check

--- Scripts/Toyota_GetServiceOperation_JSON/Script1537435010922.groovy
+++ Scripts/Toyota_GetServiceOperation_JSON/Script1537435010922.groovy
@@ -2,6 +2,8 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import java.lang.reflect.Array as Array
+import org.eclipse.persistence.internal.oxm.record.json.JSONParser.array_return as array_return
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
@@ -11,38 +13,31 @@ import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
 import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testobject.RequestObject
-import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.RequestObject as RequestObject
+import com.kms.katalon.core.testobject.ResponseObject as ResponseObject
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.testobject.TestObjectProperty
+import com.kms.katalon.core.testobject.TestObjectProperty as TestObjectProperty
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 
-// FIRST STEP
-// load test request object which will do the login step and retrieve the token for later usages
-RequestObject getTokenTestObject = findTestObject('Toyota/Toyota_Logon')
+//
+RequestObject GetServiceOperation = findTestObject('Toyota/GetServiceOperations_JSON', [('Dealer_Code') : GlobalVariable.Glb_Dealer_Code, ('Location_Code') : GlobalVariable.Glb_Location_Code, ('VIN') : GlobalVariable.Glb_VIN
+            , ('ServiceBay_Type') : GlobalVariable.Glb_ServiceBay_Type])
 
-ResponseObject responseTokenObject = WS.sendRequest(getTokenTestObject)
+GetServiceOperation.getHttpHeaderProperties().add(new TestObjectProperty('Authorization', ConditionType.EQUALS, 'Basic ' + 
+    GlobalVariable.Glb_Authorization_Token))
 
-// suppose that the responseTokenObject.getResponseText() is { accessToken: "xxx", expiredTime: "yyy" }
-// use groovy.json.JsonSlurper to parse the text into object
-def tokenObject = new groovy.json.JsonSlurper().parseText(responseTokenObject.getResponseText())
+ResponseObject res_GetServiceOperation = WS.sendRequest(GetServiceOperation)
 
-// get the retrieved token
-token = tokenObject.access_token
+//Verify Response Status = 200 OK
+WS.verifyResponseStatusCode(res_GetServiceOperation, 200)
 
-// SECOND STEP
-// load test request object which will use token above in Authorization
-RequestObject mainrequest = findTestObject('Toyota/GetServiceOperations_JSON')
+//Verify Booking ID
+//WS.verifyElementPropertyValue(res_GetServiceOperation, 'BookingID', GlobalVariable.Glb_Booking_ID)
 
-// if getUserInfoTestObject HTTP headers have no Authorization item
-mainrequest.getHttpHeaderProperties().add(new TestObjectProperty("Authorization", ConditionType.EQUALS, "Bearer " + token))
-
-
-response =  WS.sendRequest(mainrequest)
-WS.verifyResponseStatusCode(response, 200)
