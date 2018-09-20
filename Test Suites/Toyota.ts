fb3b368a8ead5261bbfc77cc8d21bc2d4ec70ddf
nhien.do <nhien.do@TITAN-V-10.titandms.local>
2018-09-20 10:45:35 +0700

    try to commit to newbranch

--- Test Suites/Toyota.ts
+++ Test Suites/Toyota.ts
@@ -4,7 +4,7 @@
    <name>Toyota</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-19T13:46:01</lastRun>
+   <lastRun>2018-09-19T15:46:13</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
